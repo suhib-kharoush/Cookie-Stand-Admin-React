656c39f57db6f8395e29b3403145e6cc57daab8b
suhib kharoush <suhaib_kharwash@outlook.com>
2021-08-02 18:23:51 +0300

    first commit

--- components/footer.js
+++
@@ -1,7 +0,0 @@
-export default function Footer(){
-    return (
-        <footer  className = "p-5 text-left bg-green-500 h-15">
-            2021 &copy;
-        </footer>
-    )
-}
--- components/header.js
+++
@@ -1,7 +0,0 @@
-export default function CookieStandHeader(){
-    return (
-        <header class = "text-3xl text-left bg-green-500">
-            <h1>Cookie Stand Admin</h1>
-        </header>
-    )
-}
